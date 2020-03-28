fb6763eff73e9ecf517e7a718252cf76eaab0b8e
Kiryl Lutsyk <zetsubou.zero.0@gmail.com>
2020-03-28 17:27:35 +0300

    maven dependencies plugin has been added message to identify path during processing has been added

--- src/main/java/com/zetsubou_0/parser/ParserApp.java
+++ src/main/java/com/zetsubou_0/parser/ParserApp.java
@@ -12,7 +12,9 @@ public class ParserApp {
             System.err.println("Please specify file name");
             return;
         }
-        final Injector injector = Guice.createInjector(new ParserModules(args[0]));
+        final String path = args[0];
+        System.out.println("Output csv files will be saved under " + path + " folder");
+        final Injector injector = Guice.createInjector(new ParserModules(path));
         final ParserRunner parserRunner = injector.getInstance(ParserRunner.class);
         new Thread(parserRunner).start();
     }
